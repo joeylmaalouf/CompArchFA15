c4484c923719969c307703d8b4df5dbfc8957c4c
joeylmaalouf <joeylmaalouf@gmail.com>
2015-09-20 14:11:30 -0400

    formatting

--- HW/HW2/multiplexer.v
+++ HW/HW2/multiplexer.v
@@ -1,16 +1,16 @@
 module behavioralMultiplexer(out, address0,address1, in0,in1,in2,in3);
-output out;
-input address0, address1;
-input in0, in1, in2, in3;
-wire[3:0] inputs = {in3, in2, in1, in0};
-wire[1:0] address = {address1, address0};
-assign out = inputs[address];
+  output out;
+  input address0, address1;
+  input in0, in1, in2, in3;
+  wire[3:0] inputs = {in3, in2, in1, in0};
+  wire[1:0] address = {address1, address0};
+  assign out = inputs[address];
 endmodule
 
 module structuralMultiplexer(out, address0,address1, in0,in1,in2,in3);
-output out;
-input address0, address1;
-input in0, in1, in2, in3;
+  output out;
+  input address0, address1;
+  input in0, in1, in2, in3;
   // Your multiplexer code here
 endmodule
 
