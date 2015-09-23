7174bae361d9b30409bf7b5e432e44e67677e02d
joeylmaalouf <joeylmaalouf@gmail.com>
2015-09-23 12:12:53 -0400

    wrote tests

--- HW/HW2/adder.v
+++ HW/HW2/adder.v
@@ -16,6 +16,22 @@ module testFullAdder;
   behavioralFullAdder adder (sum, carryout, a, b, carryin);
 
   initial begin
-    // Your test code here
+    $display("A B Ci | Co S | Expected Output");
+    a = 0; b = 0; carryin = 0; #1000 
+    $display("%b %b  %b |  %b %b | Both False", a, b, carryin, carryout, sum);
+    a = 0; b = 1; carryin = 0; #1000 
+    $display("%b %b  %b |  %b %b | Sum True", a, b, carryin, carryout, sum);
+    a = 1; b = 0; carryin = 0; #1000 
+    $display("%b %b  %b |  %b %b | Sum True", a, b, carryin, carryout, sum);
+    a = 1; b = 1; carryin = 0; #1000 
+    $display("%b %b  %b |  %b %b | CarryOut True", a, b, carryin, carryout, sum);
+    a = 0; b = 0; carryin = 1; #1000 
+    $display("%b %b  %b |  %b %b | Sum True", a, b, carryin, carryout, sum);
+    a = 0; b = 1; carryin = 1; #1000 
+    $display("%b %b  %b |  %b %b | CarryOut True", a, b, carryin, carryout, sum);
+    a = 1; b = 0; carryin = 1; #1000 
+    $display("%b %b  %b |  %b %b | CarryOut True", a, b, carryin, carryout, sum);
+    a = 1; b = 1; carryin = 1; #1000 
+    $display("%b %b  %b |  %b %b | Both True", a, b, carryin, carryout, sum);
   end
 endmodule
--- HW/HW2/multiplexer.v
+++ HW/HW2/multiplexer.v
@@ -1,4 +1,4 @@
-module behavioralMultiplexer(out, address0,address1, in0,in1,in2,in3);
+module behavioralMultiplexer(out, address0, address1, in0, in1, in2, in3);
   output out;
   input address0, address1;
   input in0, in1, in2, in3;
@@ -7,7 +7,7 @@ module behavioralMultiplexer(out, address0,address1, in0,in1,in2,in3);
   assign out = inputs[address];
 endmodule
 
-module structuralMultiplexer(out, address0,address1, in0,in1,in2,in3);
+module structuralMultiplexer(out, address0, address1, in0, in1, in2, in3);
   output out;
   input address0, address1;
   input in0, in1, in2, in3;
@@ -16,5 +16,23 @@ endmodule
 
 
 module testMultiplexer;
-  // Your test code here
+  reg address0, address1;
+  reg in0, in1, in2, in3;
+  wire out;
+  integer i;
+  behavioralMultiplexer multiplexer (out, address0, address1, in0, in1, in2, in3);
+
+  initial begin
+    $display("A1 A0 | I0 I1 I2 I3 | O | Expected Output");
+    for (i = 0; i < 64; i = i + 1) begin
+      address1 = i[5];
+      address0 = i[4];
+      in0 = i[3];
+      in1 = i[2];
+      in2 = i[1];
+      in3 = i[0];
+      #1000
+      $display(" %b  %b |  %b  %b  %b  %b | %b | Input%d", address1, address0, in0, in1, in2, in3, out, 2 * i[5] + 1 * i[4]);
+    end
+  end
 endmodule
